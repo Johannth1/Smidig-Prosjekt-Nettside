32b607342dacbd3f4e60caf25a28687b8edc290d
Laon21 <31162065+Laon21@users.noreply.github.com>
2019-05-29 12:29:13 +0200

    fixed headerbar

--- src/Client/headerbar.jsx
+++ src/Client/headerbar.jsx
@@ -17,14 +17,14 @@ export class HeaderBar extends React.Component {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Navbar.Brand>
-                    <Link to={"/game"}>
-                        <img
-                            src={logo}
-                            width="40"
-                            height="40"
-                            className="d-inline-block align-top"
-                            alt="Tidvis logo"
-                        />
+                    <Link to="/"> 
+                    <img
+                        src={logo}
+                        width="40"
+                        height="40"
+                        className="d-inline-block align-top"
+                        alt="Tidvis logo"
+                    />
                     </Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -32,7 +32,7 @@ export class HeaderBar extends React.Component {
                     <Nav className="mr-auto">
                         <Nav.Link href="https://www.tidvis.no/" target="_blank">Om Tidvis</Nav.Link>
                         <Nav.Link href="https://oslohavn1798.no/" target="_blank">Oslo Havn 1798</Nav.Link>
-                    </Nav>                   
+                    </Nav>
                 </Navbar.Collapse>
             </Navbar >
         );
